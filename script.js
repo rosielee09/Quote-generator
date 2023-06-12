9a93981672f2a610e64424b0d7d72bf83cc2914d
rosie <rosie.bh.lee@gmail.com>
2023-06-11 19:32:20 -0700

    add generating newquote using DOM

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// Get Quotes from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
@@ -8,9 +12,22 @@ let apiQuotes = [];
 function newQuote() {
   // Pcik a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Check if author field is balck and replace it with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 100) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
 }
 
+// Get Quotes from API
 async function getQquotes() {
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
@@ -18,9 +35,9 @@ async function getQquotes() {
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    
-    // Catch Error
+    // Catch Error Here
   }
 }
 
+// On load
 getQquotes();
